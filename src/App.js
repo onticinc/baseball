26191fa89152f08fadfc1ffdf667dcfec20b472a
onticinc <onticinc@gmail.com>
2021-12-19 16:39:29 -0700

    fixed mistake on app.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,6 @@ import Card from './Card';
 // import Equipment from './Equipment';
 import React from 'react';
 
-// Equipment
-const equipmentInfo = [
-  {
-      name: 'Bat',
-      description: '',
-      belongsTo: 'Player for the mariners in the 90s',
-  },
-  {
-      name: 'Ken Griffy Jr.',
-      team: 'Mariners',
-      about: 'Player for the mariners in the 90s',
-  },
-  {
-      name: 'King Felix',
-      team: 'Mariners',
-      about: 'Player for the mariners in the 90s',
-  },
-];
-
-
-
 function App() {
   return (
     <div className="App">
